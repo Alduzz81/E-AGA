26ece7369b1cf15e73ed68e81428aab839cf1666
aldo.carlino <aldo.carlino@avanade.com>
2016-05-20 10:55:01 +0200

    aa

--- eagaproject/bundle/src/main/java/com/aem/eaga/sightly/registration/Registration.java
+++ eagaproject/bundle/src/main/java/com/aem/eaga/sightly/registration/Registration.java
@@ -1,8 +1,38 @@
 package com.aem.eaga.sightly.registration;
  
 import com.adobe.cq.sightly.WCMUse;
+import javax.jcr.Node;
+import com.aem.eaga.common.CommonUtility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Registration  extends WCMUse  {
+	  private static final Logger LOG = LoggerFactory.getLogger(Registration.class);
+	 private Node currentNode;
+	    private String firstMsg;
+	    private String labelUtente;
+	    private String labelEmail;
+	    private String labelPassword;
+	    private String labelPassword2;
+	    private String labelButtonReq;
+	    private String txtInformativa;
 	 public void activate() throws Exception {
-		 
+		 LOG.debug("====================== LoginCta Activate Start ======================");
+	       
+	        currentNode = get("currentNode", Node.class);
+	        if (currentNode == null) {
+	            return;
+	        }
+	        
+	        firstMsg = CommonUtility.getPropertyValue(currentNode, "firstMsg");
+	        labelUtente = CommonUtility.getPropertyValue(currentNode, "labelUtente");
+	        labelEmail = CommonUtility.getPropertyValue(currentNode, "labelEmail");
+	        labelPassword = CommonUtility.getPropertyValue(currentNode, "labelPassword");
+	        labelPassword2 = CommonUtility.getPropertyValue(currentNode, "labelPassword2");
+	        labelButtonReq = CommonUtility.getPropertyValue(currentNode, "labelButtonReq");
+	        txtInformativa = CommonUtility.getPropertyValue(currentNode, "txtInformativa");
+
+	        
+	        LOG.debug("====================== LoginCta Activate End ======================");
 	 }
 }
--- eagaproject/content/src/main/content/jcr_root/apps/eaga/components/page/page/body.jsp
+++ eagaproject/content/src/main/content/jcr_root/apps/eaga/components/page/page/body.jsp
@@ -2,6 +2,7 @@
 <body>
 	<div class="content-wrapper ">
 		<cq:include script="header.jsp"/>
+		da qui
 		<cq:include script="content.jsp"/>
 		<cq:include script="footer.jsp"/>
 	</div>
