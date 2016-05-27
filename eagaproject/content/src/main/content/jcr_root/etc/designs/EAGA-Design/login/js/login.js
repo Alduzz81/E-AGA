ebfb700bb65334fe5189370b446542f209a11f24
giuseppe.gaglione <giuseppe.gaglione@avanade.com>
2016-05-27 17:13:09 +0200

    Complete login

--- eagaproject/content/src/main/content/jcr_root/etc/designs/EAGA-Design/login/js/login.js
+++ eagaproject/content/src/main/content/jcr_root/etc/designs/EAGA-Design/login/js/login.js
@@ -1,44 +1,33 @@
-//var eagaApp = angular.module('eagaApp', []);
-
 eagaApp.controller("LoginController", ['$scope', '$window', function($scope,$window) {
-    // I contain the details of the customer to be rendered.
-	$scope.utenti=1;
-	$scope.loginValidate() = function() {
-		$window.location.href = '/content/eaga/products.html';
+	$scope.email="";
+	$scope.password="";
+	$scope.valid=true;
+	$scope.loginValidate = function(e,p) {
+		$scope.valid=true;
+		var i=0;
+		while(i<$scope.utenti.length){
+			if(($scope.utenti[i].email==$scope.email) && ($scope.utenti[i].password==$scope.password)){
+				$window.location.href = '/content/eaga/products.html';
+				return;
+			}
+			i++;
+		}
+		$scope.valid=false;
     };
-}]);
-
-
-/*
-eagaApp.controller("LoginController", ['$scope', function($scope) {  
-	$scope.utenti1=1;
-	$scope.fullName = function() {
-		console.log("Ciao");
-		$scope.utenti1=5;
-    };
-	
-	$scope.loginAccount= function(){
-		$scope.utenti1=5;
-	}
-	
-	/*
-	$scope.utenti =   
+    
+    $scope.utenti =   
 	    [
 			{
 				email:"eaga1@eag.it",
-				passwrod:"123"
+				password:"123"
 			},
 			{
 				email:"eaga2@eag.it",
-				passwrod:"123"
+				password:"123"
 			},
 			{
 				email:"eaga3@eag.it",
-				passwrod:"123"
+				password:"123"
 			}
 		]
-}]);*/
-/*
-function loginAccount(){
-	console.log("Ciao");
-}*/
+}]);
