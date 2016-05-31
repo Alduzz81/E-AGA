ade4aa46761c710872474f95d63418aa84229bab
giuseppe.gaglione <giuseppe.gaglione@avanade.com>
2016-05-31 12:16:59 +0200

    cookie

--- eagaproject/eaga-content/src/main/content/jcr_root/etc/designs/EAGA-Design/main/js/cookie-policy.js
+++ eagaproject/eaga-content/src/main/content/jcr_root/etc/designs/EAGA-Design/main/js/cookie-policy.js
@@ -1,12 +1,16 @@
 var cookieValueP=false;
+
+//document.cookie = "COOKIE_NAME=" false; 
 function initNotification () {	
-	//console.log("cok1: "+cookieValueP);
-	$('div.notification').addClass("notification-opened");
+	//console.log("cok1: "+getCookieNames());
+	//if(document.cookie==false)
+		$('div.notification').addClass("notification-opened");
 }
 
 $("#notification-close").on("click", function () {
 	var cookieValueP=true;
-//	console.log("cok3: "+cookieValueP);
+	//document.cookie = "COOKIE_NAME=" true;
+	//console.log("cok3: "+getCookieNames());
 	$('div.notification').removeClass("notification-opened");
 });
 
