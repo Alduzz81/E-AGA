998cffee245205fd58ef937989e88479049b74c7
maria aramil <aizle.aramil@avanade.com>
2016-06-08 12:59:36 +0200

    Added the Add Product Page and the addproduct component.

---
+++ eagaproject/eaga-content/src/main/content/jcr_root/etc/designs/EAGA-Design/products/js/addproduct.js
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * START ADD PRODUCT
+ ******************************************************************************/
+var notready;
+function formEmpty(sel,mes){
+	$(sel).addClass('product-form-empty');
+	$(sel).val(mes);
+	notready = true;
+};
+
+var productSubmit = function(){
+	notready=false;
+
+	if($('#productname').val() == '' ){
+        formEmpty('#productname', 'Inserisci un nome.');
+	}if($('#productcategory').val() == '' ){
+        formEmpty('#productcategory', 'Inserisci una categoria.');
+	}if($('#productdescription').val() == '' ){
+		formEmpty('#productdescription', 'Inserisci una descrizione.');
+	}if($('#productprice').val() == '' ){
+		formEmpty('#productprice', 'Inserisci il prezzo.');
+	}if($('#productquantity').val() == '' ){
+		formEmpty('#productquantity', 'Inserisci la quantita.');
+	} if(notready === false){
+		addProductInDB();
+	}
+};
+function addProductInDB() {
+    var params = {
+	        'j_nomeProdotto': $("input[name=productname]").val(),
+	        'j_categoriaProdotto': $("input[name=productcategory]").val(),
+	        'j_descrizioneProdotto': $("input[name=productdescription]").val(),
+	        'j_prezzoProdotto': $("input[name=productprice]").val(),
+	        'j_quantitaProdotto': $("input[name=productquantity]").val()
+    	};
+    var path = CQ.shared.HTTP.getPath();
+
+    $.ajax({
+        type: 'GET',
+        url: path + '.InsertProduct.json',
+        data: params,
+        success: function (msg) {
+        	console.log('Add product success '+ status);
+        	window.alert("Product added");
+        },
+        error: function (data, status) {
+            console.log('Procedure failed: ' + status);
+            window.alert("Something went wrong");
+        }
+    });
+};
+	
+/*******************************************************************************
+ * END ADD PRODUCT
+ ******************************************************************************/
+
+
+$(document).ready(function () {
+    $('input').on('dblclick',function() {
+		$(this).val('');
+        $(this).removeClass('product-form-empty');
+	});
+});
