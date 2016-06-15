925966707a257f6af05591dcc5d19bec888a1354
Andrea Chiesa <andrea.chiesa@avanade.com>
2016-06-15 17:54:08 +0200

    cart page almost completed (added angular functions into page)

--- eagaproject/eaga-content/src/main/content/jcr_root/etc/designs/EAGA-Design/cart/js/cart.js
+++ eagaproject/eaga-content/src/main/content/jcr_root/etc/designs/EAGA-Design/cart/js/cart.js
@@ -2,7 +2,235 @@
  * START CART
  ******************************************************************************/
 
+function goToCheckout(){
+	alert("ciao");
+}
 
+var list = {
+		Prodotto_1: {
+					IdProdotto: 1,
+					NomeProdotto: "Pippo",
+					DescrizioneProdotto: "baudo",
+					PrezzoProdotto: 1256,
+					QuantitaProdotto: 2,
+					QuantitaSelezionata: 1,
+					CategoriaProdotto: "minchioni"
+					},
+		Prodotto_2: {
+					IdProdotto: 2,
+					NomeProdotto: "TestServlet",
+					DescrizioneProdotto: "descrizioneServlet",
+					PrezzoProdotto: 102,
+					QuantitaProdotto: 99,
+					QuantitaSelezionata: 1,
+					CategoriaProdotto: "Servlet"
+					},
+		Prodotto_3: {
+					IdProdotto: 3,
+					NomeProdotto: "Iphone6s",
+					DescrizioneProdotto: "Descrizione",
+					PrezzoProdotto: 499,
+					QuantitaProdotto: 50,
+					QuantitaSelezionata: 2,
+					CategoriaProdotto: "Telefono"
+					},
+		Prodotto_4: {
+					IdProdotto: 4,
+					NomeProdotto: "Iphone 5",
+					DescrizioneProdotto: "iphone5",
+					PrezzoProdotto: 399,
+					QuantitaProdotto: 50,
+					QuantitaSelezionata: 1,
+					CategoriaProdotto: "Telefono"
+					},
+		Prodotto_5: {
+					IdProdotto: 5,
+					NomeProdotto: "Iphone5",
+					DescrizioneProdotto: "iphone5",
+					PrezzoProdotto: 399,
+					QuantitaProdotto: 50,
+					QuantitaSelezionata: 1,
+					CategoriaProdotto: "Telefono"
+					},
+		Prodotto_6: {
+					IdProdotto: 6,
+					NomeProdotto: "Iphone4",
+					DescrizioneProdotto: "iphone4",
+					PrezzoProdotto: 299,
+					QuantitaProdotto: 100,
+					QuantitaSelezionata: 1,
+					CategoriaProdotto: "Telefono"
+					},
+		Prodotto_7: {
+					IdProdotto: 7,
+					NomeProdotto: "Iphone 6",
+					DescrizioneProdotto: "4322",
+					PrezzoProdotto: 432,
+					QuantitaProdotto: 23,
+					QuantitaSelezionata: 1,
+					CategoriaProdotto: "423"
+					},
+		Prodotto_8: {
+					IdProdotto: 8,
+					NomeProdotto: "Iphone6",
+					DescrizioneProdotto: "656",
+					PrezzoProdotto: 23,
+					QuantitaProdotto: 30,
+					QuantitaSelezionata: 3,
+					CategoriaProdotto: "565"
+					},
+		Prodotto_9: {
+					IdProdotto: 9,
+					NomeProdotto: "Samsung S5",
+					DescrizioneProdotto: "descrizione",
+					PrezzoProdotto: 349,
+					QuantitaProdotto: 100,
+					QuantitaSelezionata: 1,
+					CategoriaProdotto: "Telefono"
+					},
+		Prodotto_10: {
+					IdProdotto: 10,
+					NomeProdotto: "S4",
+					DescrizioneProdotto: "f",
+					PrezzoProdotto: 600,
+					QuantitaProdotto: 3,
+					QuantitaSelezionata: 1,
+					CategoriaProdotto: "f"
+					},
+		Prodotto_11: {
+					IdProdotto: 11,
+					NomeProdotto: "S3",
+					DescrizioneProdotto: "td",
+					PrezzoProdotto: 89,
+					QuantitaProdotto: 5,
+					QuantitaSelezionata: 3,
+					CategoriaProdotto: "fdt"
+					},
+		Prodotto_12: {
+					IdProdotto: 12,
+					NomeProdotto: "S2",
+					DescrizioneProdotto: "w",
+					PrezzoProdotto: 5,
+					QuantitaProdotto: 5,
+					QuantitaSelezionata: 1,
+					CategoriaProdotto: "w"
+					},
+		Prodotto_13: {
+					IdProdotto: 13,
+					NomeProdotto: "maria",
+					DescrizioneProdotto: "sdfsdfsd",
+					PrezzoProdotto: 2312,
+					QuantitaProdotto: 22,
+					QuantitaSelezionata: 2,
+					CategoriaProdotto: "3333"
+					}
+	};
+
+
+eagaApp.controller("CartController", ['$scope', '$window', function($scope, $window) {
+	
+	$scope.saluta = function(){
+		alert("ciaooo!!!! typeof list: "+ typeof list);
+	};
+	
+	$scope.cartList = list;
+	
+	$scope.options = function(qnt){
+		var arr = [];
+		if(qnt < 10){
+			for(var i = 1; i <= qnt ; i++){
+				arr.push(i);
+			}
+		} else {
+			arr = [1,2,3,4,5,6,7,8,9,10];
+		}
+		return arr;
+	}
+	
+	$scope.findItem = function(id){
+		for(var key in $scope.cartList){
+			if($scope.cartList[key].IdProdotto == id){
+				return $scope.cartList[key];
+			}
+		}
+	}
+	
+	$scope.total = function(){
+		var tot = 0;
+		for(var i in $scope.cartList){
+			var obj = $scope.cartList[i];
+			tot += obj.PrezzoProdotto * obj.QuantitaSelezionata;
+		}
+		return tot;
+	};
+	
+	$scope.totalQnt = function(){
+		var tot = 0;
+		for(var i in $scope.cartList){
+			tot += parseInt($scope.cartList[i].QuantitaSelezionata);
+		}
+		return tot;
+	};
+	
+	$scope.wishList = {};
+	
+	/*$scope.removeProduct = function(id){
+		var i = $scope.cartList.findIndex(function(item){
+			return item.id === id ;
+		});
+		$scope.cartList.splice(i, 1);
+	};
+	
+	$scope.moveToSavedForLater = function(id){
+		var i = $scope.cartList.findIndex(function(item){
+			return item.id === id ;
+		});
+		$scope.cartList.splice(i, 1);
+	};*/
+	
+	$scope.removeProduct = function(id){
+		for(var key in $scope.cartList){
+			if($scope.cartList[key].IdProdotto == id){
+				delete $scope.cartList[key];
+			}
+		}
+	};
+	
+	$scope.moveToSavedForLater = function(id){
+		for(var key in $scope.cartList){
+			if($scope.cartList[key].IdProdotto == id){
+				$scope.wishList[key] = $scope.cartList[key];
+				delete $scope.cartList[key];
+			}
+		}
+	};
+	
+	$scope.removeWishProduct = function(id){
+		for(var key in $scope.wishList){
+			if($scope.wishList[key].IdProdotto == id){
+				delete $scope.wishList[key];
+			}
+		}
+	};
+	
+	$scope.moveToCart = function(id){
+		for(var key in $scope.wishList){
+			if($scope.wishList[key].IdProdotto == id){
+				$scope.cartList[key] = $scope.wishList[key];
+				delete $scope.wishList[key];
+			}
+		}
+	};
+	
+	$scope.totalWishQnt = function(){
+		var tot = 0;
+		for(var i in $scope.wishList){
+			tot++;
+		}
+		return tot;
+	};
+	
+}]);
 
 /*******************************************************************************
  * CART AT DOCUMENT READY
