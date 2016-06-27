ca023fa119116af62717126b954e35ee739d7a8f
giuseppe.gaglione <giuseppe.gaglione@avanade.com>
2016-06-27 10:24:14 +0200

    change ip db

--- eagaproject/eaga-presentation/src/main/java/com/aem/eaga/common/DbUtility.java
+++ eagaproject/eaga-presentation/src/main/java/com/aem/eaga/common/DbUtility.java
@@ -13,12 +13,18 @@ public class DbUtility {
     private final Logger LOG = LoggerFactory.getLogger(getClass());
     public Connection getConnection() throws ClassNotFoundException, SQLException {
     	 Class.forName("com.mysql.jdbc.Driver");
-   		 Connection conn = DriverManager.getConnection("jdbc:mysql://10.107.104.16/eaga?" 
-   				 		   + "user=eaga&password=eaga" );
+    	 Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/eaga?" 
+			 		   + "user=root&password=admin" );
    		 return conn;
    		
-  		 //Aldo's ip 10.107.104.16, user=eaga&password=eaga
-   		 //mine ip 127.0.0.1 
-  		 
+   		/*Aldo
+   		 Connection conn = DriverManager.getConnection("jdbc:mysql://10.107.104.16/eaga?" 
+		 		   + "user=eaga&password=eaga" );
+		 
+		 Giuseppe
+		 Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/eaga?" 
+   				 		   + "user=root&password=admin" );
+		*/
+   		 
     }
 }
